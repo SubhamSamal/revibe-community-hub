a64323fb83e2c5ce44dc36bbca4130b38c3f7c41
Subham Samal <subham.samal17@gmail.com>
2025-06-14 13:22:02 +0530

    Android app added

---
+++ android/app/src/main/java/com/revibe/communityhub/MainActivity.java
@@ -0,0 +1,5 @@
+package com.revibe.communityhub;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,18 +1,17 @@
-
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'app.lovable.974b7be1981c4f378edb6a381907d0e6',
-  appName: 'revibe-community-hub',
+  appId: 'com.revibe', // ✅ Valid Java package format
+  appName: 'Revibe',
   webDir: 'dist',
-  server: {
-    url: 'https://974b7be1-981c-4f37-8edb-6a381907d0e6.lovableproject.com?forceHideBadge=true',
-    cleartext: true
-  },
+  // server: {
+  //   url: 'https://974b7be1-981c-4f37-8edb-6a381907d0e6.lovableproject.com?forceHideBadge=true',
+  //   cleartext: true
+  // },
   plugins: {
     SplashScreen: {
       launchShowDuration: 2000,
-      backgroundColor: '#1d1d23',
+      backgroundColor: '#eee6d7',
       showSpinner: false
     }
   }
